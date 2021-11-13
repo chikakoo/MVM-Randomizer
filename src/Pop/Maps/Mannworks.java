f4fbc70df98568ac338f8ccd7c3c7a9de9c6423b
Lionel Crafts <lionel.crafts@gmail.com>
2021-11-13 12:25:03 -0600

    Make Mannworks and Skullcove use ironman on wave 666; fix the relay names on Mannworks

--- src/Pop/Maps/Mannworks.java
+++ src/Pop/Maps/Mannworks.java
@@ -1,6 +1,8 @@
 package Pop.Maps;
 
 import Pop.Enums.TankRoutes;
+import Pop.MVMRandomizer;
+import Pop.PopObject;
 
 /**
  * Mannworks
@@ -18,5 +20,36 @@ public class Mannworks extends Map {
         tankRoutes.add(TankRoutes.MANNWORKS_1);
         tankRoutes.add(TankRoutes.MANNWORKS_2);
         useOnKilledOutput = false;
+    }
+
+    /**
+     * Creates the object for StartWaveOutput - this is the same for each wave
+     * If wave 666, sets the bomb so that it can respawn on its own
+     * @return the created object
+     */
+    public PopObject createStartWaveOutputObject() {
+        PopObject startWaveOutput = new PopObject("StartWaveOutput");
+
+        if (MVMRandomizer.botSettings.isWave666Mode()) {
+            startWaveOutput.addAttribute("Target", "bombpath_arrows_clear_relay_ironman");
+        } else {
+            startWaveOutput.addAttribute("Target", "bombpath_arrows_clear_relay");
+        }
+
+        startWaveOutput.addAttribute("Action", "Trigger");
+
+        return startWaveOutput;
+    }
+
+    /**
+     * Creates the object for DoneOutput - this is the same for each wave
+     * @return the created object
+     */
+    public PopObject createDoneOutputObject() {
+        PopObject doneOutput = new PopObject("DoneOutput");
+        doneOutput.addAttribute("Target", "bombpath_wavefinished");
+        doneOutput.addAttribute("Action", "Trigger");
+
+        return doneOutput;
     }
 }
--- src/Pop/Maps/Skullcove.java
+++ src/Pop/Maps/Skullcove.java
@@ -3,6 +3,8 @@ package Pop.Maps;
 
 import Pop.Enums.SpawnLocations;
 import Pop.Enums.TankRoutes;
+import Pop.MVMRandomizer;
+import Pop.PopObject;
 
 public class Skullcove extends Map {
     /**
@@ -27,5 +29,24 @@ public class Skullcove extends Map {
 
         tankRoutes.add(TankRoutes.SKULLCOVE_1);
         tankRoutes.add(TankRoutes.SKULLCOVE_2);
+    }
+
+    /**
+     * Creates the object for StartWaveOutput - this is the same for each wave
+     * If wave 666, sets the bomb so that it can respawn on its own
+     * @return the created object
+     */
+    public PopObject createStartWaveOutputObject() {
+        PopObject startWaveOutput = new PopObject("StartWaveOutput");
+
+        if (MVMRandomizer.botSettings.isWave666Mode()) {
+            startWaveOutput.addAttribute("Target", "wave_start_relay_ironman");
+        } else {
+            startWaveOutput.addAttribute("Target", "wave_start_relay");
+        }
+
+        startWaveOutput.addAttribute("Action", "Trigger");
+
+        return startWaveOutput;
     }
 }
